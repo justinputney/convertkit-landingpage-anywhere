24fdaf0c466deaf3ed0d07cfab434025ddaafb0d
justinputney <justin.putney@gmail.com>
2020-05-15 12:38:50 -0700

    Removed simple-html-dom dependency
    Was no longer working in php 7.4

--- convertkit/landingpage.php
+++ convertkit/landingpage.php
@@ -11,50 +11,42 @@ function page_takeover($landing_page_url) {
 
 function get_resource($url) {
     $resource = '';
+$test = array();
 	if(!empty($url)) {
-		$response = get_request($url);
-		if(!function_exists('str_get_html')) {
-		  require_once(dirname(__FILE__).'/../vendor/simple-html-dom/simple-html-dom.php');
-		}
+		$response = file_get_contents($url);
 		if(!function_exists('url_to_absolute')) {
 		  require_once(dirname(__FILE__).'/../vendor/url-to-absolute/url-to-absolute.php');
 		}
-		$url_parts = parse_url($url);
-		//$body = wp_remote_retrieve_body($response);
-		$body = $response;
-		$html = str_get_html($body);
-		foreach($html->find('a, link') as $element) {
-		  if(isset($element->href) && $element->href[0] != "#") {
-			$element->href = url_to_absolute($url, $element->href);
+		$doc = new DOMDocument();
+		libxml_use_internal_errors(TRUE); //disable libxml errors
+
+		$doc->loadHTML($response);
+		libxml_clear_errors(); //remove errors for yucky html
+		$doc_xpath = new DOMXPath($doc);
+		
+		foreach($doc_xpath->query('//a | //link') as $element) {
+			$href = $element->getAttribute('href');
+		  if(isset($href) && strlen($href) && $href[0] != "#") {
+			$element->setAttribute('href', url_to_absolute($url, $href) );
 		  }
 		}
-		foreach($html->find('img, script') as $element) {
-		  if(isset($element->src)) {
-			$element->src = url_to_absolute($url, $element->src);
+		foreach($doc_xpath->query('//img | //script')  as $element) {
+			$src = $element->getAttribute('src');
+		  if(isset($src) && strlen($src)) {
+			$element->setAttribute('src', url_to_absolute($url, $src) );
 		  }
 		}
-		foreach($html->find('form') as $element) {
-		  if(isset($element->action)) {
-			$element->action = url_to_absolute($url, $element->action);
+		foreach($doc_xpath->query('//form')  as $element) {
+			$action = $element->getAttribute('action');
+		  if(isset($action) && strlen($action)) {
+			$element->setAttribute('action', url_to_absolute($url, $action) );
 		  } else {
-			$element->action = $url;
+			$element->setAttribute('action', $url);
 		  }
 		}
-		$resource = $html->save();
+		$resource = $doc->saveHTML();
 	  }
     return $resource;
-}
-
-function get_request($url) {
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-    $results = curl_exec($ch);
-    curl_close($ch);
-    return $results;
 }
 
 ?>
